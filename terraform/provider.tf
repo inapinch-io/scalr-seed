4ba48d4d94b3f2c29a5003ada58b3bcfc5f4a793
inapinch-io <123926196+inapinch-io@users.noreply.github.com>
2023-11-08 22:55:54 +0000

    create base resources

---
+++ terraform/provider.tf
@@ -0,0 +1,10 @@
+terraform {
+    required_providers {
+        scalr = {
+            source  = "registry.scalr.io/scalr/scalr"
+            version = "~> 1.0"
+        }
+    }
+}
+
+provider "scalr" {}
